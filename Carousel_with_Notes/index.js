1be6299902ca15c0f9b49406204645b802230e63
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-11-15 20:39:02 +0530

    added

--- Carousel_with_Notes/index.js
+++ Carousel_with_Notes/index.js
@@ -5,4 +5,14 @@
 https://www.freecodecamp.org/news/javascript-timing-events-settimeout-and-setinterval/
 
 
+
+--> setTimeout
+    setTimeout() is used to delay the execution of the passed 
+    function by a specified amount of time.
+
+    setTimeout(function, time_in_milliseconds ) ex- 1000
+
+    now that function will excute after 1 second only once...
+
+
 */
